6819180cf8c4f0508ba4cc8ad15d705d6b8d604e
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-05-10 14:20:43 +0800

    fix hasher

--- yrs/src/utils/client_hasher.rs
+++ yrs/src/utils/client_hasher.rs
@@ -18,13 +18,9 @@ impl Hasher for ClientHasher {
 
     #[inline]
     fn write(&mut self, bytes: &[u8]) {
-        debug_assert!(bytes.len() <= 8);
+        debug_assert!(bytes.len() == 16);
         // we only expect a single value to be written
         debug_assert!(self.prefix == 0);
-        self.prefix = if bytes.len() == 4 {
-            panic!("ClientHasher: 32 bit client-ids are not supported!");
-        } else {
-            u64::from_ne_bytes(<[u8; 8]>::try_from(bytes).unwrap())
-        };
+        self.prefix = u64::from_ne_bytes(<[u8; 8]>::try_from(bytes).unwrap());
     }
 }
